be7328dfa1e729e35fe9dc92c57205b27e585804
Shancm <ruan-shan@163.com>
2018-06-03 08:15:24 +0800

    first init

---
+++ servletcase/src/main/java/org/shancm/webcase/servletcase/DemoServlet.java
@@ -0,0 +1,33 @@
+package org.shancm.webcase.servletcase;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class DemoServlet implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("init");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        System.out.println("getServletConfig");
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("service");
+    }
+
+    @Override
+    public String getServletInfo() {
+        System.out.println("getServletInfo");
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+}
